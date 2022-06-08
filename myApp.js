6f22edca8e575e6ebbb9caa38860c9d75506917d
TrashBinNp2019 <49650767+TrashBinNp2019@users.noreply.github.com>
2022-06-08 10:51:30 +0300

    added nosniff

--- myApp.js
+++ myApp.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 app.use(helmet.frameguard({ action: 'deny' }));  
 app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 module.exports = app;
 const api = require('./server.js');
